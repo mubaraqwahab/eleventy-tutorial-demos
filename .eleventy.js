b3d8f80a4f691dc3c29f7785b551650f669ad2ad
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-10-22 22:18:07 +0100

    prettier nuisance

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,9 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/js");
   return {
     htmlTemplateEngine: "njk",
     dir: {
-      input: "src"
-    }
+      input: "src",
+    },
   };
 };
