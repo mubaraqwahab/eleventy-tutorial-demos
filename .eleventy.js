e2adf42ba85a05b185888bd9e3d653b2a477bb0b
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-10-23 16:46:09 +0100

    Create blog stub

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,15 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
+
+  eleventyConfig.addFilter("readableDate", (/** @type {Date} */ dateObj) => {
+    return dateObj.toLocaleDateString(undefined, {
+      year: "numeric",
+      month: "long",
+    });
+  });
+
+  eleventyConfig.setFrontMatterParsingOptions({ excerpt: true });
+
   return {
     htmlTemplateEngine: "njk",
     dir: {
